24b17320a02ec9c13a8d58e9b913b4bc46447acb
ThVinicius <vinicioss9955@gmail.com>
2023-01-25 14:23:16 -0300

    feat: schemaValidator for body and query

--- src/middlewares/schemaValidator.ts
+++ src/middlewares/schemaValidator.ts
@@ -1,9 +1,14 @@
 import { Request, Response, NextFunction } from 'express'
 import { ObjectSchema } from 'joi'
 
-function schemaValidator(schema: ObjectSchema) {
+function schemaValidator(schema: ObjectSchema, isQuery?: boolean) {
+  let payload
+
   return (req: Request, res: Response, next: NextFunction) => {
-    const { error } = schema.validate(req.query, { abortEarly: false })
+    if (isQuery) payload = req.query
+    else payload = req.body
+
+    const { error } = schema.validate(payload, { abortEarly: false })
 
     if (error) {
       return res.status(400).send(error.details.map(detail => detail.message))
