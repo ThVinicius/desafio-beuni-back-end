2d13651b8f530a7ef3ea6fa5cd204a82a36e16fa
ThVinicius <vinicioss9955@gmail.com>
2023-01-26 20:52:14 -0300

    feat: query  type

--- src/repositories/cartRepository.ts
+++ src/repositories/cartRepository.ts
@@ -1,12 +1,12 @@
 import prisma from '../database/prisma'
-import { ICart } from '../types/cartType'
+import { ICart, ICartQuery } from '../types/cartType'
 
 async function add(data: ICart) {
   return await prisma.cart.create({ data })
 }
 
 async function findAllByCustomerId(customerId: number) {
-  return await prisma.$queryRaw`
+  return await prisma.$queryRaw<ICartQuery[]>`
     SELECT c.id AS "cartId", p.id AS "productId", p.name, 
       p.image, c.quantity, c."totalPrice" 
     FROM carts c
