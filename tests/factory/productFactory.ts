8753391c982457cfd1b378f3a91cc2fb478a724d
ThVinicius <vinicioss9955@gmail.com>
2023-01-27 14:16:54 -0300

    feat: product factory

---
+++ tests/factory/productFactory.ts
@@ -0,0 +1,16 @@
+export function productDbFactory() {
+  return {
+    id: 1,
+    name: 'camiseta',
+    category: 'camiseta',
+    description: 'camiseta',
+    image: 'camiseta',
+    brand: 'camiseta',
+    hasFreeShipping: true,
+    rating: 5,
+    minimumQuantity: 50,
+    kitManagerVariations: 'camiseta',
+    stock: 100,
+    price: 20
+  }
+}
