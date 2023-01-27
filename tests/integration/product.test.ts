ec2824bc007c14833379d92eca3a0fee314770bc
ThVinicius <vinicioss9955@gmail.com>
2023-01-27 13:33:46 -0300

    fix: product tests

--- tests/integration/product.test.ts
+++ tests/integration/product.test.ts
@@ -1,11 +1,17 @@
 import agent from '../config/supertestConfig'
+import { customerTest } from './../../prisma/seed/customerSeed'
 
 describe('GET /products tests', () => {
   it('deve retornar todos os produtos cadastrados', async () => {
     const productsCount = 50
     const OK = 200
 
-    const { status, body } = await agent.get('/products')
+    const signIn = await agent.post('/sign-in').send(customerTest)
+    const { token } = signIn.body
+
+    const { status, body } = await agent
+      .get('/products')
+      .set('Authorization', `Bearer ${token}`)
 
     expect(status).toEqual(OK)
     expect(body.length).toEqual(productsCount)
@@ -15,7 +21,12 @@ describe('GET /products tests', () => {
     const expectedReturn = 9
     const OK = 200
 
-    const { status, body } = await agent.get('/products?name=camiseta')
+    const signIn = await agent.post('/sign-in').send(customerTest)
+    const { token } = signIn.body
+
+    const { status, body } = await agent
+      .get('/products?name=camiseta')
+      .set('Authorization', `Bearer ${token}`)
 
     expect(status).toEqual(OK)
     expect(body.length).toEqual(expectedReturn)
